cf8bbec9d0d3284e771a3efc71ab5fd65c8c5be8
12157591+AplUSAndmINUS@users.noreply.github.com <terence.waters@imail.org>
2020-03-15 15:15:53 -0600

    finished factorial logic and bootstrap styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Button, Col, Container, Form, Row, Jumbotron } from 'react-bootstrap';
 
 const App = () => {
-	return <h1>This is my app!</h1>;
+	const [state, setState] = React.useState({
+		number: ''
+	});
+	const [factorial, setFactorial] = React.useState('');
+
+	const calculateFactorial = (e: any) => {
+		e.preventDefault();
+		let factorial = 1;
+		let num = parseInt(state.number);
+		if (isNaN(num)) return null;
+
+		for (let i = 1; i <= num; i++) {
+			factorial *= i;
+		}
+
+		setFactorial(`Factorial of ${num}! is: ${factorial}`);
+	};
+
+	return (
+		<>
+			<Jumbotron>
+				<h1>Factoralize a number</h1>
+				<p>Takes a number from an input and factoralizes it</p>
+			</Jumbotron>
+			<Container fluid>
+				<Row>
+					<Col md={6}>
+						<Form onSubmit={calculateFactorial}>
+							<Form.Group controlId='formNumber'>
+								<Form.Label>Enter a number:</Form.Label>
+								<Form.Control
+									type='text'
+									value={state.number}
+									name='number'
+									onChange={(e: any) => setState({ number: e.target.value })}
+								/>
+							</Form.Group>
+						</Form>
+						<Button type='submit' variant='primary'>
+							Submit
+						</Button>{' '}
+						<Button
+							type='reset'
+							variant='outline-secondary'
+							onClick={() => {
+								setState({
+									number: ''
+								});
+								setFactorial('');
+							}}
+						>
+							Clear
+						</Button>
+					</Col>
+				</Row>
+				<p>&nbsp;</p>
+				<Row p-25>
+					<Col md='auto'>
+						<p>{factorial}</p>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default App;
